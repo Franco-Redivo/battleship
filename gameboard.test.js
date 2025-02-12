a67ae897697be2f78232d5b2e92e1a1c01b36922
Franco-Redivo <franco161101@gmail.com>
2025-02-12 19:04:32 -0300

    placeShip function unit tests

---
+++ gameboard.test.js
@@ -0,0 +1,49 @@
+import { Gameboard } from './gameboard.js';
+
+test('Gameboard has a board and ships', () => {
+    const gameboard = new Gameboard();
+    expect(gameboard.board).toEqual({
+        1: Array(10).fill(null),
+        2: Array(10).fill(null),
+        3: Array(10).fill(null),
+        4: Array(10).fill(null),
+        5: Array(10).fill(null),
+        6: Array(10).fill(null),
+        7: Array(10).fill(null),
+        8: Array(10).fill(null),
+        9: Array(10).fill(null),
+        10: Array(10).fill(null)
+    });
+    expect(gameboard.ships).toEqual([]);
+});
+
+test('Gameboard can place a ship', () => {
+    const gameboard = new Gameboard();
+    gameboard.placeShip(0, 0, 3, 'horizontal');
+    expect(gameboard.ships[0].coordinates).toEqual([
+        [0, 0],
+        [0, 1],
+        [0, 2]
+    ]);
+    gameboard.placeShip(1, 1, 3, 'vertical');
+    expect(gameboard.ships[1].coordinates).toEqual([
+        [1, 1],
+        [2, 1],
+        [3, 1]
+    ]);
+});
+
+test('Gameboard cant place a ship on top of another ship', () => {
+    const gameboard = new Gameboard();
+    gameboard.placeShip(0, 0, 3, 'horizontal');
+    gameboard.placeShip(0, 0, 3, 'horizontal');
+    expect(gameboard.ships.length).toBe(1);
+});
+
+test('Gameboard cant place a ship outside the board', () => {
+    const gameboard = new Gameboard();
+    gameboard.placeShip(9, 9, 3, 'horizontal');
+    expect(gameboard.ships.length).toBe(0);
+    gameboard.placeShip(9, 9, 3, 'vertical');
+    expect(gameboard.ships.length).toBe(0);
+});
