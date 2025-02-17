c1ba89fdaa07555a035eea76a520c89fedfda33e
Franco-Redivo <franco161101@gmail.com>
2025-02-17 17:56:32 -0300

    render win or lose message

--- gameLogic.js
+++ gameLogic.js
@@ -3,6 +3,7 @@ import { Player } from './player.js';
 
 let playerGrid = document.querySelector('#playerGrid');
 let enemyGrid = document.querySelector('#enemyGrid');
+let main = document.querySelector('main');
 
 export function startGame(player, enemy){
 
@@ -10,15 +11,54 @@ export function startGame(player, enemy){
         if(e.target.classList.contains('enemy-cell')){
             let x = e.target.dataset.x;
             let y = e.target.dataset.y;
+
+            if(e.target.classList.contains('played')){
+                return;
+            }
             enemy.receiveAttack(x, y);
             clearEnemyGrid();
             generateEnemyGrid(enemy.gameboard);
+            
             if(enemy.gameboard.allShipsSunk()){
-                alert('You win!');
+                renderWin();
+                renderPlayAgain();
             }
+            
+        
             player.receiveAttackRandomly();
             clearPlayerGrid();
             generatePlayerGrid(player.gameboard);
+
+            if(player.gameboard.allShipsSunk()){
+                renderLose();
+                renderPlayAgain();
+            }
         }
     });
+}
+
+function renderWin(){
+    let winDiv = document.createElement('div');
+    winDiv.classList.add('win');
+    winDiv.textContent = 'You win!';
+    main.appendChild(winDiv);
+    
+}
+
+function renderLose(){
+    let loseDiv = document.createElement('div');
+    loseDiv.classList.add('lose');
+    loseDiv.textContent = 'You lose!';
+    main.appendChild(loseDiv);
+    
+}
+
+function renderPlayAgain(){
+    let playAgainBtn = document.createElement('button');
+    playAgainBtn.classList.add('play-again');
+    playAgainBtn.textContent = 'Play Again';
+    playAgainBtn.addEventListener('click', () => {
+        location.reload();
+    });
+    main.appendChild(playAgainBtn);
 }
